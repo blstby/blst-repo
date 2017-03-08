1748289ae794f6e0d8ac6f9e2df04d36e8a33994
blstby <najid.re93@gmail.com>
2017-03-08 16:43:58 +0100

    Add files via upload
    ajout du script de création de la BD cible

---
+++ TiersAffaire_Oracle_Client/src/main/resources/test.sql
@@ -0,0 +1,18 @@
+-- phpMyAdmin SQL Dump
+-- version 4.6.5.2
+-- https://www.phpmyadmin.net/
+--
+-- Client :  127.0.0.1
+-- Généré le :  Mer 08 Mars 2017 à 16:44
+-- Version du serveur :  10.1.21-MariaDB
+-- Version de PHP :  5.6.30
+
+--
+-- Base de données :  `test`
+--
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+
+CREATE DATABASE test;
